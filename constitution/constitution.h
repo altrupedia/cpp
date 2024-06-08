f6ab8b74b3f49fc3460fbc321416b30f0a1221e1
Stephan Friedrich Stiefelmaier <stephan.friedrich.stiefelmaier@cern.ch>
2024-06-08 14:59:10 +0200

    add callable prototype code for constitution/

--- constitution/constitution.h
+++ constitution/constitution.h
@@ -27,22 +27,76 @@ Altruist State:
 - conceptional studies will partly be carried out by the non-profit organisation altrEDUpedia eV: 
 - could be an interesting Jugend forscht or bachelelor thesis multidimensional data analysis topic: 
   Come up a good approximative description of the underlying dimensionality reduction mechanism, without knowing it
+*/
+# pragma once
+#include <set>
+#include <string>
+
+namespace altrupedia_constitution 
+{
+
+// defining all commands callable through the API
+class S_commands {  
+  private:
+    enum E_cmd {
+    kRegisterUser,
+    kDeleteUser,
+    kUnknown
+  };
+
+  public: 
+    static int Execute(std::string const &theCommand);
+  
+  private:
+    static E_cmd hashit (std::string const& inString);
+    static int executeRegisterUser();
+    static int executeDeleteUser();
+};
+
+class S_Door {
+  public:
+    static int Bounces(std::string const &theRequest); 
+  private:
+};
+
 
 enum E_altruist {
-  kRegisteredAnonymously
-  kGreenhorn
-  kRealnameRevelationVerificationProcessStarted
-  kRealnameVettedAndRevealed
-  ...
-  ...
+  kRegisteredAnonymously,
+  kGreenhorn,
+  kRealnameRevelationVerificationProcessStarted,
+  kRealnameVettedAndRevealed,
+  // ...
+  // ...
   kAltruist 
   /**/
 };
 
+/* the user representation within the c++ kernel*/
+class T_altruist {
+    public:
+    
+    private:
+        //E_altruist fState;    
+};
+
+
+/*
+base class for instances of Altruversums. 
+They define the environments in which users live, can participate, kids will live in one adults on another
+properties:
+- they live forever until deleted, which will be exceptional
+
 */
-namespace altrupedia_constitution 
-{
-  
-}
+class T_altruversum {
+  public:
+
+  private:
+    std::string fID;
+    std::string const fTimeOfCreation; // string for now
+    std::set<T_altruist> fCitizens;
+
+};  // T_altruversum
+
+} // namespace altrupedia_constitution 
 
 
